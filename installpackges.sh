fa005e9c29d1315b5e94b673c37d6cc5db8baafb
varun <varunkumarreddy1879@gmail.com>
2025-06-25 23:35:55 +0530

    installing tools

--- installpackges.sh
+++ installpackges.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+set -e
 
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2...$G SUCCESS $N"
-    fi
+failure(){
+    echo "Failed at $1: $2"
 }
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u) #ERR
 
 if [ $USERID -ne 0 ]
 then
@@ -27,15 +18,7 @@ else
     echo "You are super user."
 fi
 
-for i in $@
-do
-    echo "package to install: $i"
-    dnf list installed $i &>>$LOGFILE
-    if [ $? -eq 0 ]
-    then
-        echo -e "$i already installed...$Y SKIPPING $N"
-    else
-        dnf install $i -y &>>$LOGFILE
-        VALIDATE $? "Installation of $i"
-    fi
-done
+dnf install mysfaffql -y
+dnf install git -y
+
+echo "is script proceeding?"
